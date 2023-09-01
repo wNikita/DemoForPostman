7c2e5ecfb8545f5e5856c1b22596d710b1d0ae22
Nikita <nrw@narola.email>
2023-09-01 17:30:07 +0530

    changes

--- src/main/java/com/example/postmandemo/CORSFilter.java
+++ src/main/java/com/example/postmandemo/CORSFilter.java
@@ -14,11 +14,9 @@ import java.io.IOException;
 
 public class CORSFilter extends OncePerRequestFilter {
 
-    private final Logger LOG = LoggerFactory.getLogger(CORSFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
-        LOG.info("Adding CORS Headers ........................");
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         res.setHeader("Access-Control-Allow-Headers", "*");
