39cdaf7e2fa2ae1cc96fa03b4666c923b5f9ed39
Nikita <nrw@narola.email>
2023-09-01 12:43:44 +0530

    changes

--- src/main/java/com/example/postmandemo/ServletInitializer.java
+++ src/main/java/com/example/postmandemo/ServletInitializer.java
@@ -21,9 +21,9 @@ public class ServletInitializer extends AbstractAnnotationConfigDispatcherServle
         return null;
     }
 
-//    @Override
-//    protected Filter[] getServletFilters() {
-//        return new Filter[]{ new CORSFilter()};
-//    }
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{ new CORSFilter()};
+    }
 
 }
